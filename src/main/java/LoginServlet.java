9cd8c077d5c08c70839c4780073548d6227af0f9
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-01-22 15:23:06 -0600

    fixed issues with exercise??

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -10,14 +10,7 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getMethod().equalsIgnoreCase("post")) {
-            String username = req.getParameter("username");
-            String password = req.getParameter("password");
-            if (username.equals("admin") && password.equals("password")) {
-                resp.sendRedirect("/ads");
-            }
-
-        }
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
     }
 
     @Override
@@ -27,10 +20,9 @@ public class LoginServlet extends HttpServlet {
             String password = req.getParameter("password");
             if (username.equals("admin") && password.equals("password")) {
                 resp.sendRedirect("/ads");
+            }else{
+                resp.sendRedirect("/login");
             }
-
         }
-
-//        req.getRequestDispatcher("/login.jsp").forward(req, resp);
     }
 }
