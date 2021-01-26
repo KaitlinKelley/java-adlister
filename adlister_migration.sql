0ab2551353c4d135c07c6c42430bbbfb58900361
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-01-26 12:30:15 -0600

    created config, updated gitignore, made migration file

---
+++ adlister_migration.sql
@@ -0,0 +1,23 @@
+USE adlister_db;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+DROP TABLE IF EXISTS ads;
+
+CREATE TABLE IF NOT EXISTS ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(50),
+    description VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+)
