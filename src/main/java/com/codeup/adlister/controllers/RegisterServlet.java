e8b53e5eb9bfe84a661135918f3e5f1f2418c27d
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-01-28 16:15:21 -0600

    worked on exercise

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,19 +1,63 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.MySQLUsersDao;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-        // TODO: show the registration form
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/ads/register.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        String submittedUsername = request.getParameter("username");
+        String submittedEmail = request.getParameter("email");
+        String submittedPassword = request.getParameter("password");
+
+            try {
+                if(isValidPassword(submittedPassword) && isUsernameAvailable(submittedUsername)) {
+                    User user = new User(
+                            submittedUsername,
+                            submittedEmail,
+                            submittedPassword
+                    );
+                    DaoFactory.getUsersDao().insert(user);
+                    HttpSession session = request.getSession();
+                    session.setAttribute("user", user);
+                    request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+                }else{
+                    response.sendRedirect("/register");
+                }
+            } catch (IOException | ServletException e) {
+                throw new RuntimeException("Error occurred in doPost on RegisterServlet", e);
+            }
+    }
+
+
+
         // TODO: ensure the submitted information is valid
         // TODO: create a new user based off of the submitted information
         // TODO: if a user was successfully created, send them to their profile
+
+
+
+    public static boolean isValidPassword(String password){
+        String regex = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])(?=\\S+$).{8,}";
+        return password.matches(regex);
     }
+
+    public static boolean isUsernameAvailable(String username){
+        return DaoFactory.getUsersDao().findByUsername(username) == null;
+    }
+
 }
--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,35 +1,78 @@
-//package com.codeup.adlister.dao;
-//
-//import com.codeup.adlister.models.User;
-//import com.mysql.cj.jdbc.Driver;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-//
-//public class MySQLUsersDao implements Users{
-//
-//    private Connection connection = null;
-//
-//    public MySQLUsersDao(Config config) {
-//        try {
-//            DriverManager.registerDriver(new Driver());
-//            connection = DriverManager.getConnection(
-//                    config.getUrl(),
-//                    config.getUser(),
-//                    config.getPassword()
-//            );
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Error connecting to the database!", e);
-//        }
-//    }
-//
-////    User findByUsername(String username){
-////        return username;
-////    }
-//
-//
-////    Long insert(User user){
-////
-////    }
-//}
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users{
+
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    public User findByUsername(String username) {
+        String sql = "SELECT * FROM users WHERE username LIKE ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            if(!rs.next()){
+                return null;
+            }
+            return new User(
+                    rs.getLong("id"),
+                    rs.getString("username"),
+                    rs.getString("email"),
+                    rs.getString("password")
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error running findByUsername", e);
+        }
+
+    }
+
+
+
+    public Long insert(User user) {
+        try {
+            PreparedStatement stmt = createInsertQuery(user);
+            stmt.setLong(1, user.getId());
+            stmt.setString(2, user.getUsername());
+            stmt.setString(3, user.getEmail());
+            stmt.setString(4, user.getPassword());
+
+            stmt.executeUpdate();
+            ResultSet idResultSet = stmt.getGeneratedKeys();
+
+            idResultSet.next();
+            return idResultSet.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new user.", e);
+        }
+    }
+
+
+    private PreparedStatement createInsertQuery(User user) {
+        String sql = "INSERT INTO users(id, username, email, password) VALUES (?, ?, ?, ?)";
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error at createInsertQuery", e);
+        }
+        return stmt;
+    }
+}
